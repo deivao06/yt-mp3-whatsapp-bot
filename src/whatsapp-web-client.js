7bb6f3d7a980e9875213ff5703320c1277203c1a
deivao06 <david.moura12@hotmail.com>
2024-01-10 17:10:52 -0300

    wwebjs local auth

--- src/whatsapp-web-client.js
+++ src/whatsapp-web-client.js
@@ -29,6 +29,7 @@ class WhatsappWebClient {
         ];
 
         this.wwebClient = new Client({
+            authStrategy: new LocalAuth(),
             ffmpegPath: ffmpeg,
             puppeteer: {
                 executablePath: chromiumBinary.path,
